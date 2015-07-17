eefc88d5730aeed094a10ad72d0e452943b7795f
charlestang <charlestang@foxmail.com>
2015-07-17 12:24:12 +0800

    logdb test

--- tests/logdb/LogDbTest.php
+++ tests/logdb/LogDbTest.php
@@ -11,7 +11,7 @@ class LogDbTest extends PHPUnit_Framework_TestCase
     public function testSend()
     {
         $conf = [
-            'id'        => 'int',
+            'test_id'        => 'int',
             'key_name'  => 'varchar',
             'key_value' => 'text',
             'update'    => 'datetime',
@@ -19,7 +19,7 @@ class LogDbTest extends PHPUnit_Framework_TestCase
 
         $logdb = charlestang\commonlib\logdb\LogDb::getInstance('172.16.57.128', '22060', $conf);
         $ret   = $logdb->send([
-            'key_name'  => 'this is a test to key insert',
+            'key_name'  => 'this is a key',
             'key_value' => 'this the body value of test content',
         ]);
 
