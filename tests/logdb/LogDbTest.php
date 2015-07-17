1995368732566806301d82fb0059d3fa6aca0b7b
charlestang <charlestang@foxmail.com>
2015-07-17 13:46:00 +0800

    change the logdb unit test

--- tests/logdb/LogDbTest.php
+++ tests/logdb/LogDbTest.php
@@ -11,10 +11,8 @@ class LogDbTest extends PHPUnit_Framework_TestCase
     public function testSend()
     {
         $conf = [
-            'test_id'   => 'int',
             'key_name'  => 'varchar',
             'key_value' => 'text',
-            'update'    => 'datetime',
         ];
 
         $logdb = charlestang\commonlib\logdb\LogDb::getInstance('172.16.57.128', '22060', $conf);
